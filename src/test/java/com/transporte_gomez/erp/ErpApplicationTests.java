cd9ab05c837f752553f4c05b036fc21f5bc13d9d
Victor Moya <vmanuelmoya@gmail..com>
2025-05-31 16:36:32 -0400

    first commit

---
+++ src/test/java/com/transporte_gomez/erp/ErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transporte_gomez.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
